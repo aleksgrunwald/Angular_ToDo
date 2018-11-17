12370b5fd5b25774cb17c6b5593f31a5faa6c067
Aleksandra <aleks.grunwald@gmail.com>
2018-11-17 14:41:25 +0100

    Simsple test with event binding

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,17 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   template: `
-    <button>
-      CLICK MEE!
-    </button>
+    <p>
+      input-button-unit works!
+      The title is: {{ title }}
+    </p>
+​
+    <input [value]="title" id="input1">
+    <button (click)="changeTitle('CLECKID!')">save</button>
   `,
   styleUrls: ['./input-button-unit.component.sass']
 })
 export class InputButtonUnitComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
   }
 
+    title: string
+
+    generateTitle(): string {
+      return 'Passed by component - title'
+    }
+
+    changeTitle(msg) {
+      this.title=msg;
+    }
 }
